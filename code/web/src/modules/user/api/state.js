e29ac685769bb38cbd2d5e9390d49942ea045b7a
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-12-06 22:40:38 -0700

    Add annotations for reducers

--- code/web/src/modules/user/api/state.js
+++ code/web/src/modules/user/api/state.js
@@ -1,8 +1,25 @@
+//This file contains our reducers that update state
+  //they take in our action objects & return a specific part
+  //of state depending on the action type 
+
+
 // App Imports
 import { isEmpty } from '../../../setup/helpers'
 import { SET_USER, LOGIN_REQUEST, LOGIN_RESPONSE, LOGOUT } from './actions'
 
 // Initial State
+
+//For displaying a user's received & kept items:
+  //We'll need to add a new property to a user here
+  //Something like: 
+  //itemsReceived: {
+  //   1: true,
+  //   2: false,
+  //   3: true,
+  //    etc...
+  // }
+//Need to communicate with the backend to figure out how to store this info
+
 export const userInitialState = {
   error: null,
   isLoading: false,
