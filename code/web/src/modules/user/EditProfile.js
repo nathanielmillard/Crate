08baf078441dea70a278b0d2a602c43318020619
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-12-09 16:09:12 -0700

    Create EditProfile with initial render

---
+++ code/web/src/modules/user/EditProfile.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+
+
+class EditProfile extends Component {
+    constructor() {
+        super()
+    }
+
+    render() {
+        return(
+            <p>Profile goes here</p>
+        )
+    }
+}
+
+export default EditProfile
