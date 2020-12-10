9f315eedb739f2d58b84ca8c314baca30563b988
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-12-09 17:00:51 -0700

    Add input fields

--- code/web/src/modules/user/EditProfile.js
+++ code/web/src/modules/user/EditProfile.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 
 
 class EditProfile extends Component {
-    constructor() {
+    constructor(props) {
         super()
         this.state = {
             //image: ?
@@ -16,9 +17,41 @@ class EditProfile extends Component {
 
     render() {
         return(
-            
+            <form>
+                <input
+                    type='text'
+                    placeholder='name'
+                    value={this.state.name}
+                    //onChange
+                />
+                <input
+                    type='text'
+                    placeholder='description'
+                    value={this.state.description}
+                    //onChange
+                />
+                <input
+                    type='text'
+                    placeholder='email'
+                    value={this.state.email}
+                    //onChange
+                />
+                <input
+                    type='text'
+                    placeholder='address'
+                    value={this.state.address}
+                    //onChange
+                />
+            </form>
         )
     }
 }
 
-export default EditProfile
+// Component State
+function editProfileState(state) {
+    return {
+      user: state.user
+    }
+  }
+
+export default connect(editProfileState)(EditProfile)
