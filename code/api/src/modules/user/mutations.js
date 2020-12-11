c801eb07e0c6e65e7494fd2d713ab6c329b2ae7d
Travis McKinstry <mckinstrytg@gmail.com>
2020-12-11 11:44:53 -0700

    Rebasing main into test file
    Co-authored-by: Eric <EHale64@github.com>
    Co-authored-by: Tyler <edfields93@gmail.com>

--- code/api/src/modules/user/mutations.js
+++ code/api/src/modules/user/mutations.js
@@ -3,9 +3,15 @@ import { GraphQLString, GraphQLInt } from 'graphql'
 
 // App Imports
 import { UserType } from './types'
+<<<<<<< HEAD
 import { create, remove, edit } from './resolvers'
+=======
+import { create, remove } from './resolvers'
+// The 'import' commands above import functions & variables from other files/folders
+>>>>>>> 0667a8c... Create a markdown file to describe which files will need to be updated/changed for the specific track. Also adds annotations to each file that will be updated/changed to describe what the file is currently doing
 
 // Create
+// This is where the action of User.create is defined. If 'User.create' is called in the project, the following code is executed, requiring the 'args' to not be null:
 export const userSignup = {
   type: UserType,
   args: {
@@ -28,6 +34,7 @@ export const userSignup = {
 }
 
 // Remove
+// This is the command that is called when the code 'User.remove' is made
 export const userRemove = {
   type: UserType,
   args: {
