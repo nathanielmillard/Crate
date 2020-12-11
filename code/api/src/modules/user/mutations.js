fe4b20479dfcc8aff5393bc4f3888c11d9b15a5a
Travis McKinstry <mckinstrytg@gmail.com>
2020-12-11 11:50:35 -0700

    Removed unnecessary annotations
    Co-authored-by: Tyler Fields <tdfields93@gmail.com>
    Co-authored-by: Eric Hale <EHale@github.com>

--- code/api/src/modules/user/mutations.js
+++ code/api/src/modules/user/mutations.js
@@ -8,7 +8,6 @@ import { create, remove, edit } from './resolvers'
 
 
 // Create
-// This is where the action of User.create is defined. If 'User.create' is called in the project, the following code is executed, requiring the 'args' to not be null:
 export const userSignup = {
   type: UserType,
   args: {
@@ -31,7 +30,6 @@ export const userSignup = {
 }
 
 // Remove
-// This is the command that is called when the code 'User.remove' is made
 export const userRemove = {
   type: UserType,
   args: {
