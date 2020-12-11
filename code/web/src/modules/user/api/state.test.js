36948767bf9bc8e8d66a35070ba53a68d1184cba
Kathryn Jackson <kathryn.lorraine.j@gmail.com>
2020-12-10 21:44:37 -0500

    Pass Test: should set user state

--- code/web/src/modules/user/api/state.test.js
+++ code/web/src/modules/user/api/state.test.js
@@ -1,4 +1,5 @@
 import state from './state.js'
+import { SET_USER, LOGIN_REQUEST, LOGIN_RESPONSE, LOGOUT } from './actions'
 
 describe('state', () => {
     it('should return the initial state', () => {
@@ -11,6 +12,31 @@ describe('state', () => {
         }
         const result = state(undefined, {});
         expect(result).toEqual(initialState);
+
+    })
+
+    it('should set the user state', () => {
+        const action ={
+            type: SET_USER,
+            user: 'forehead Taylor'
+        }
+
+        const initialState = {
+            error: null,
+            isLoading: false,
+            isAuthenticated: false,
+            details: null
+        }
+        const nextState = state(initialState, action);
+
+        expect(nextState).toEqual(
+            {
+                details: 'forehead Taylor',
+                error: null,
+                isAuthenticated: true,
+                isLoading: false
+            }
+        );
 
     })
 })
