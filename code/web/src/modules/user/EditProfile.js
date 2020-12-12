e1f15130b80e4a39d98a6961f15022deb1e5a19c
Kathryn Jackson <kathryn.lorraine.j@gmail.com>
2020-12-12 15:17:57 -0500

    Format Button and Input, changed tags to match

--- code/web/src/modules/user/EditProfile.js
+++ code/web/src/modules/user/EditProfile.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { updateUserInfo } from './api/actions'
 
+// UI Imports
+import ImageTile from '../../ui/image/Tile'
+import { Grid, GridCell } from '../../ui/grid'
+import { H3, H4 } from '../../ui/typography'
+import Button from '../../ui/button'
+import Input from '../../ui/input/Input'
+import { grey, grey2 } from '../../ui/common/colors'
+import { level1 } from '../../ui/common/shadows'
+
 
 class EditProfile extends Component {
     constructor(props) {
@@ -26,36 +35,36 @@ class EditProfile extends Component {
 
     render() {
         return(
-            <form>
-                <input
+            <form style={{ padding: '2em', textAlign: 'center' }}>
+                <Input
                     type='text'
                     name='name'
                     placeholder='name'
                     value={this.state.name}
                     onChange={event => this.handleUserInput(event)}
                 />
-                <input
+                <Input
                     type='text'
                     name='description'
                     placeholder='description'
                     value={this.state.description}
                     onChange={event => this.handleUserInput(event)}
                 />
-                <input
+                <Input
                     type='text'
                     name='email'
                     placeholder='email'
                     value={this.state.email}
                     onChange={event => this.handleUserInput(event)}
                 />
-                <input
+                <Input
                     type='text'
                     name='address'
                     placeholder='address'
                     value={this.state.address}
                     onChange={event => this.handleUserInput(event)}
                 />
-                <button type='button' onClick={this.updateUser}>Submit</button>
+                <Button theme="primary" style={{ marginLeft: '1em' }} type='button' onClick={this.updateUser}>Submit</Button>
                 {/* onClick, will need to dispatch action*/}
             </form>
         )
